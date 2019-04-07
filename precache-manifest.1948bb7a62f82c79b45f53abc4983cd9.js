ff34b2e48ac6bf48998f031adb1fcf143044a674
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-07 15:19:18 -0400

    Updates

---
+++ precache-manifest.1948bb7a62f82c79b45f53abc4983cd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "70d467ac3f83f0093e36",
+    "url": "/Sfg/static/css/main.2bdf96a9.chunk.css"
+  },
+  {
+    "revision": "70d467ac3f83f0093e36",
+    "url": "/Sfg/static/js/main.a4d61ae7.chunk.js"
+  },
+  {
+    "revision": "18e1afaef4a16ec985ab",
+    "url": "/Sfg/static/js/runtime~main.63a52e0a.js"
+  },
+  {
+    "revision": "ef3efdd9509041c56e13",
+    "url": "/Sfg/static/js/2.4b7f161a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sfg/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9abb6922f2049a21f9ef0e2d7135d9fb",
+    "url": "/Sfg/static/media/phoenix.9abb6922.png"
+  },
+  {
+    "revision": "dd5b9179ade71b3b842a23df23c384af",
+    "url": "/Sfg/index.html"
+  }
+];
